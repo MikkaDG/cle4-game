93c26161bed87daeaac4f5b9592cb6914e29e7ef
mikedegr <mike-de-graaff@hotmail.com>
2023-06-15 13:40:40 +0200

    game canvas is centered

--- src/js/game.js
+++ src/js/game.js
@@ -8,6 +8,7 @@ export class Game extends Engine {
     constructor() {
         super({ width: 1200, height: 800 })
         this.start(ResourceLoader).then(() => this.startGame())
+        ResourceLoader.suppressPlayButton = true
     }
 
     startGame() {
