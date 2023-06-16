ed196e99fab301f8a8f9a292e8611023747dd965
mikedegr <mike-de-graaff@hotmail.com>
2023-06-16 11:40:53 +0200

    onnodige player import weg uit gamescne

--- src/js/gamescene.js
+++ src/js/gamescene.js
@@ -1,16 +1,11 @@
 import '../css/style.css'
 import {Actor, Engine, Scene, Vector} from 'excalibur';
 import { Resources, ResourceLoader } from './resources.js'
-import {Player} from './player.js';
 
 export class Gamescene extends Scene {
 
         onInitialize(engine) {
                 // this.game = this.engine
-
-                //de player die wordt meegegeven vanuit het startscherm zijn position wordt opnieuw ingesteld
-                this.player = new Player()
-                this.player.pos = new Vector(150, 600)
 
         }
 }
