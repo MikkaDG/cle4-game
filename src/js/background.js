9de91c0b4727466875c194e8bf2da4432453dd57
mikedegr <mike-de-graaff@hotmail.com>
2023-06-16 11:53:38 +0200

    hoogte spelers

--- src/js/background.js
+++ src/js/background.js
@@ -2,36 +2,38 @@ import {Actor, Vector, GraphicsGroup} from 'excalibur';
 import {Resources} from './resources.js';
 
 export class Background extends Actor {
+    game;
+    offset;
 
-    offset
+    onInitialize(engine) {
+        this.backgroundImage = Resources.Background.toSprite(); // Eerste achtergrondafbeelding
+        // this.scale = new Vector(0.3, 0.3);
+        this.offset = this.backgroundImage.width;
+        this.game = engine;
 
-    onInitialize(engine){
-        const backgroundImage = Resources.Background.toSprite()
-        this.offset = backgroundImage.width
-        backgroundImage.height = 820
-        backgroundImage.width = 1900
+        this.members = [
+            {
+                graphic: this.backgroundImage,
+                pos: new Vector(0, 0),
+            },
+            {
+                graphic: this.backgroundImage,
+                pos: new Vector(this.backgroundImage.width, 0),
+            }
+        ];
+
         const group = new GraphicsGroup({
-            members: [
-                {
-                    graphic: backgroundImage,
-                    pos: new Vector(0, 0),
-                },
-                {
-                    graphic: backgroundImage,
-                    pos: new Vector(backgroundImage.width, 0),
-                }
-            ]
-        })
+            members: this.members
+        });
 
-        this.graphics.anchor = new Vector(0,0)
-        this.graphics.add(group)
-        this.pos = new Vector(0, 0)
-        this.vel = new Vector(0, 0)
+        this.graphics.anchor = new Vector(0, 0);
+        this.graphics.add(group);
+        this.pos = new Vector(0, 0);
     }
 
     onPostUpdate(engine, delta) {
         if (this.pos.x < -this.offset) {
-            this.pos = new Vector(0, 0)
+            this.pos = new Vector(0, 0);
         }
     }
 }
--- src/js/startscreen.js
+++ src/js/startscreen.js
@@ -60,7 +60,7 @@ export class Startscreen extends Scene {
 
         cerenButton.on('pointerup', () => {
             game.goToScene('gamescene');
-            game.currentScene.add(new Ceren(300, 500));
+            game.currentScene.add(new Ceren(300, 600));
         });
 
         const mickText = new Label({
@@ -81,7 +81,7 @@ export class Startscreen extends Scene {
 
         mickButton.on('pointerup', () => {
             game.goToScene('gamescene');
-            game.currentScene.add(new Mick(300, 500));
+            game.currentScene.add(new Mick(300, 600));
         });
 
         const mikeText = new Label({
@@ -102,7 +102,7 @@ export class Startscreen extends Scene {
 
         mikeButton.on('pointerup', () => {
             game.goToScene('gamescene');
-            game.currentScene.add(new Mike(300, 500));
+            game.currentScene.add(new Mike(300, 600));
         });
 
         const suhailText = new Label({
@@ -123,7 +123,7 @@ export class Startscreen extends Scene {
 
         suhailButton.on('pointerup', () => {
             game.goToScene('gamescene');
-            game.currentScene.add(new Suhail(300, 500));
+            game.currentScene.add(new Suhail(300, 620));
         });
 
 
