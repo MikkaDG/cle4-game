a91a2d55e3cd1c93048834a85e4664c338911ccb
mickb <mickbreijer@outlook.com>
2023-06-21 14:23:03 +0200

    pigeon raycasting eruit gecomment

--- src/js/pigeon.js
+++ src/js/pigeon.js
@@ -28,43 +28,37 @@ export class Pigeon extends Actor {
 
         this.graphics.use(walkLeft);
         this.vel.x = -100;
-
-
     }
 
-
-
-
-
-    onPostUpdate(engine, delta) {
-        // Definieer de raycasting-lengte en -hoek
-        const raycastLength = 100; // Lengte van de raycast-stralen
-        const raycastAngle = Math.PI / 4; // Hoek van de raycast-stralen (45 graden in dit voorbeeld)
-
-// Voeg de volgende code toe in de update-loop van de duif
-        const raycastStartLeft = this.pos.add(new Vector(-this.width / 2, 0)); // Startpunt van de linkse raycast
-        const raycastStartRight = this.pos.add(new Vector(this.width / 2, 0)); // Startpunt van de rechtse raycast
-
-        const raycastDirectionLeft = new Vector(0, 1).rotate(raycastAngle); // Richting van de linkse raycast
-        const raycastDirectionRight = new Vector(0, 1).rotate(-raycastAngle); // Richting van de rechtse raycast
-
-        const raycastEndLeft = raycastStartLeft.add(raycastDirectionLeft.scale(raycastLength)); // Eindpunt van de linkse raycast
-        const raycastEndRight = raycastStartRight.add(raycastDirectionRight.scale(raycastLength)); // Eindpunt van de rechtse raycast
-
-// Voer de raycast uit voor de linkse straal
-        const leftRaycast = this.scene.rayCast(raycastStartLeft, raycastDirectionLeft, raycastLength, (actor) => actor instanceof Ground);
-
-// Voer de raycast uit voor de rechtse straal
-        const rightRaycast = this.scene.rayCast(raycastStartRight, raycastDirectionRight, raycastLength, (actor) => actor instanceof Ground);
-
-// Controleer of beide stralen een botsing hebben met de grondacteur
-        const isOnGround = leftRaycast && rightRaycast;
-
-// Voer acties uit op basis van de grondstatus
-        if (isOnGround) {
-            // De duif loopt nog op de grond
-        } else {
-            // De duif is niet meer op de grond
-        }
-    }
+//     onPostUpdate(engine, delta) {
+//         // Definieer de raycasting-lengte en -hoek
+//         const raycastLength = 100; // Lengte van de raycast-stralen
+//         const raycastAngle = Math.PI / 4; // Hoek van de raycast-stralen (45 graden in dit voorbeeld)
+//
+// // Voeg de volgende code toe in de update-loop van de duif
+//         const raycastStartLeft = this.pos.add(new Vector(-this.width / 2, 0)); // Startpunt van de linkse raycast
+//         const raycastStartRight = this.pos.add(new Vector(this.width / 2, 0)); // Startpunt van de rechtse raycast
+//
+//         const raycastDirectionLeft = new Vector(0, 1).rotate(raycastAngle); // Richting van de linkse raycast
+//         const raycastDirectionRight = new Vector(0, 1).rotate(-raycastAngle); // Richting van de rechtse raycast
+//
+//         const raycastEndLeft = raycastStartLeft.add(raycastDirectionLeft.scale(raycastLength)); // Eindpunt van de linkse raycast
+//         const raycastEndRight = raycastStartRight.add(raycastDirectionRight.scale(raycastLength)); // Eindpunt van de rechtse raycast
+//
+// // Voer de raycast uit voor de linkse straal
+//         const leftRaycast = this.scene.rayCast(raycastStartLeft, raycastDirectionLeft, raycastLength, (actor) => actor instanceof Ground);
+//
+// // Voer de raycast uit voor de rechtse straal
+//         const rightRaycast = this.scene.rayCast(raycastStartRight, raycastDirectionRight, raycastLength, (actor) => actor instanceof Ground);
+//
+// // Controleer of beide stralen een botsing hebben met de grondacteur
+//         const isOnGround = leftRaycast && rightRaycast;
+//
+// // Voer acties uit op basis van de grondstatus
+//         if (isOnGround) {
+//             // De duif loopt nog op de grond
+//         } else {
+//             // De duif is niet meer op de grond
+//         }
+//     }
 }
